8d5eae6825e1eb19d9449f70b316f3778653f22f
DESKTOP-ODDNJG3\학생 <ycs1278@naver.com>
2023-05-26 17:10:26 +0900

    회원가입 폼 만들기와 위도 경도 찾는것 그리고 파일 API

---
+++ js/0526_4_main.js
@@ -0,0 +1,50 @@
+// let userLatitude, userLongitude, map;
+
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition((position) => {
+//       userLatitude = position.coords.latitude;
+//       userLongitude = position.coords.longitude;
+//       document.getElementById("locationCoordinates").innerHTML =
+//         "Latitude: " + userLatitude + ", Longitude: " + userLongitude;
+//       document.getElementById("showMapButton").disabled = false;
+//     });
+//   } else {
+//     document.getElementById("locationCoordinates").innerHTML =
+//       "Geolocation is not supported by this browser.";
+//   }
+// }
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+// function showMap() {
+//   if (userLatitude && userLongitude) {
+//     const userLocation = new google.maps.LatLng(userLatitude, userLongitude);
+//     map.setCenter(userLocation);
+//     const marker = new google.maps.Marker({
+//       position: userLocation,
+//       map: map,
+//     });
+//   }
+// }
+
+/////////////////////////////////////////////////////////////////////////////
+function readFile(){
+  var files=document.getElementById('input').files;
+  console.log(files);
+  console.log(files[0].name);
+  console.log(files.length);
+
+
+var file=files[0]
+var reader=new FileReader()
+reader.readAsText(file,'utf-8')
+reader.onload=function(){
+  document.getElementById('result').value =reader.result;
+}
+}
